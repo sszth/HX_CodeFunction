4ee9f6c0a7c54d08242af4840e81a691590798b4
2810688370 <2810688370@qq.com>
2023-08-31 13:30:25 +0800

    添加提权步骤

--- XManager/QtBindFunction/XFileACL.h
+++ XManager/QtBindFunction/XFileACL.h
@@ -17,4 +17,31 @@ private:
 	bool setEveryOneEA(EXPLICIT_ACCESS* ea, PSID pSID);
 	bool setAdministratorsEA(EXPLICIT_ACCESS* ea, PSID pSID);
 	bool setCurrentUserEA(EXPLICIT_ACCESS* ea, PSID pSID);
+
+	bool SetPrivilege(
+		HANDLE hToken,          // access token handle
+		LPCWSTR lpszPrivilege,  // name of privilege to enable/disable
+		BOOL bEnablePrivilege   // to enable or disable privilege
+	);
 };
+
+//
+//class EuProcess
+//{
+//public:
+//	EuProcess(bool bSetActiveFilePrivilege = true);
+//	~EuProcess();
+//	qint64 openProcessAsUser(QString process);
+//	bool openProcessAsAdmin(QString processe);
+//	BOOL IsRunasAdmin(HANDLE hProcess);
+//	DWORD GetIdByProcessName(std::wstring ProcessName);
+//	HANDLE GetHandleByProcessId(DWORD id);
+//
+//private:
+//	void changeActiveAcFilePrivilege();
+//	QString getActiveFileDir();
+//
+//private:
+//	bool m_bForAllUser = true;
+//	bool m_bSetActiveFilePrivilege = true;
+//};
